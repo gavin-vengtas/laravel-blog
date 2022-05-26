bdd545426cd79ef45f0441be6d0b053b22e8c34d
GVengtas <vengtasgc@gmail.com>
2022-05-26 15:11:16 +0200

    Add layout to views

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>My Blog</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/app.css">
-    </head>
-    <body>
+@extends('layout')
 
-        @foreach ($post as $item)
-            <article>
-                <h2><?php echo $item->title;?></h2>
-                <p>@php
-                    echo $item->excerpt;
-                @endphp</p>
-                <a href="posts/<?php echo $item->name;?>">Read More...</a>
-            </article>
-        @endforeach
-    </body>
-</html>
+@section('content')
+    @foreach ($post as $item)
+    <article>
+        <h2>{{$item->title;}}</h2>
+        <p>{{$item->excerpt;}}</p>
+        <a href="posts/{{$item->name;}}">Read More...</a>
+    </article>
+    @endforeach
+@endsection
+
+        
