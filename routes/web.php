26264068348905a55574d617c9d4d51e0069c867
GVengtas <vengtasgc@gmail.com>
2022-06-04 11:58:41 +0200

    Include user data in posts route

--- routes/web.php
+++ routes/web.php
@@ -18,26 +18,29 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    return view('posts', ['post' => Post::all()]);
+    return view('posts', [
+        'post' => Post::with('category')->with('user')->get()
+    ]);
 });
 
-// Route::get('posts/{postID}', function ($postID) {
-
-//     $post = Post::find($postID);
-
-//     if($post){
-//         return view('post', [
-//             'post'=> $post
-//         ]);
-//     }else{
-//         abort(404);
-//     }   
-// });
-
-//cleaner
 Route::get('posts/{postID:slug}', function (Post $postID) { //Post::where('slug',$slug)->first();
     return view('post', [
         'post'=> $postID
     ]);
 
 });
+
+//Alternative
+/*
+Route::get('posts/{postID}', function ($postID) {
+
+    $post = Post::find($postID);
+
+    if($post){
+        return view('post', [
+            'post'=> $post
+        ]);
+    }else{
+        abort(404);
+    }   
+});*/
