bbbcb030e85b7487d5ba25943f48a4888108751b
GVengtas <vengtasgc@gmail.com>
2022-06-04 11:57:03 +0200

    Define user-post models relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,9 +9,13 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title','excerpt','body','slug','category_id'];
+    protected $guarded = [];
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
     }
 }
