6267d67809eb060610ab82f6b6c9caa458b5306f
GVengtas <vengtasgc@gmail.com>
2022-06-17 14:58:38 +0200

    Add Post components to views

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,17 @@
-@extends('layout')
+@extends('components/layout')
 
 @section('content')
-    @foreach ($posts as $post)
-    <article>
-        <h2>{{$post->title;}}</h2>
-        <p>
-            By <a href="{{$url}}/author/{{$post->author->username;}}">{{$post->author->name;}}</a> in <a href="{{$url}}/category/{{$post->category->slug;}}">{{$post->category->name;}}</a>
-        </p>
-        <p>{{$post->excerpt;}}</p>
-        <a href="{{$url}}/post/{{$post->slug;}}">Read More...</a>
-    </article>
-    @endforeach
+@include('_posts-header')
+
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    @if ($posts->count())
+    <x-post-grid :posts="$posts"/>
+    
+    @else
+        <p class="text-center">No Posts yet. Please check back later.</p>
+    @endif    
+</main>
 @endsection
+
 
         
