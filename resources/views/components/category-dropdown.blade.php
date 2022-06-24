2fdcfbf40fbcc2ae7eadc72a5ff24261da680996
GVengtas <vengtasgc@gmail.com>
2022-06-24 15:05:11 +0200

    Add pagination

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -11,10 +11,10 @@
             </button>
         </x-slot>
 
-        <x-dropdown-item href="/" :active="request('category')==null">All</x-dropdown-item>
+        <x-dropdown-item href="/?{{http_build_query(request()->except('category','page'))}}" :active="request('category')==null">All</x-dropdown-item>
         @foreach ($categories as $category)
             <x-dropdown-item 
-                href="{{ route('home') }}/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+                href="{{ route('home') }}/?category={{ $category->slug }}&{{ http_build_query(request()->except('category','page')) }}"
                 :active="isset($currentCategory) && $currentCategory->id == $category->id"
             >{{ucwords($category->name);}}</x-dropdown-item>
         @endforeach
